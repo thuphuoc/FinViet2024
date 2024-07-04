47d8ebfd60e74fa36b9183a83da2b92304b44239
DESKTOP-B29BG06\ThuPhuoc <thuphuoc8@gmail.com>
2024-07-04 20:37:11 +0700

    update code test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,10 +2,13 @@
 import actions.common.BasePage;
 import actions.common.GlobalConstants;
 import actions.helpers.ApiHelper;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 import pojo.loaikhuyenmai.GiamGiaSPTheoBoiSo;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -26,12 +29,14 @@ public class test {
         String sku= "SKUP117883V334265";
         int maNPP=617895;
         String soDT="0368199222";
-        List<Object> gifts = null;
-        GiamGiaSPTheoBoiSo.ProductVariant productVariant = giamGiaSPTheoBoiSo.new ProductVariant(quanity,sku,maNPP,13000,1300, gifts);
+        List<Object>gift=new ArrayList<>();
+        GiamGiaSPTheoBoiSo.ProductVariant productVariant = giamGiaSPTheoBoiSo.new ProductVariant(maNPP,sku,quanity,13000,1300,gift);
 
         GiamGiaSPTheoBoiSo.Product product = giamGiaSPTheoBoiSo.new Product(117883,Arrays.asList(productVariant));
         GiamGiaSPTheoBoiSo.Cart cart = giamGiaSPTheoBoiSo.new Cart(617895,"FINVIET",Arrays.asList(product));
         GiamGiaSPTheoBoiSo.Order order = giamGiaSPTheoBoiSo.new Order("038c5c3d0a6604da8b2f19e0c28b0c17",Arrays.asList(cart),info,soDT,"MERCHANT");
+        Gson gson = new Gson();
+//        System.out.println(gson.toJson(order));
         Response rsp=apiHelper.postRequestJsonEcom(order, GlobalConstants.URL_ECOM) ;
         System.out.println(rsp.getBody().prettyPrint());
 
